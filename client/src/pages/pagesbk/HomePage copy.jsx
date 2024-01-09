94208708c2ca7ed567e596a3a51bc5b26635c2d8
devsomsak <devrleang@gmail.com>
2024-01-09 21:04:42 +0700

    feat:create react-posts-by-using-custom-hook

--- client/src/pages/pagesbk/HomePage copy.jsx
+++ client/src/pages/pagesbk/HomePage copy.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useBlogPost } from "../../contexts/useBlogPosts";
+import axios from "axios";
 
 function HomePage() {
   const navigate = useNavigate();
-  
-  const { posts, isError, isLoading, getPosts } = useBlogPost();
+
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <div>
--- client/src/pages/pagesbk/ViewPostPage copy.jsx
+++ client/src/pages/pagesbk/ViewPostPage copy.jsx
@@ -1,10 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { useBlogPost } from "../customhook/useBlogPost";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ViewPostPage() {
   const navigate = useNavigate();
-  
-  const{posts, isError, isLoading, getPosts} = useBlogPost();
+
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <div>
